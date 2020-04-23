45fceb13cd5111d5d01dcae71b590b666d172794
Bruce Xu <ganyu.x@leantaas.com>
2020-04-22 22:24:07 -0700

    Added users initial URLs

--- anniversary_project/anniversary_project/urls.py
+++ anniversary_project/anniversary_project/urls.py
@@ -3,5 +3,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('archives/', include('archive.urls')),
+    path('users/', include('users.urls')),
     path('admin/', admin.site.urls),
 ]
--- anniversary_project/users/views.py
+++ anniversary_project/users/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.http import HttpRequest, HttpResponse
+from django.contrib.auth.forms import UserCreationForm
 
-# Create your views here.
+
+def register(request: HttpRequest) -> HttpResponse:
+    user_creation_form = UserCreationForm()
+    return render(request, 'users/register.html', {'user_creation_form': user_creation_form})
