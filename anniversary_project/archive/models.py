602d0f1fcb73255677c0f16b21a5d76fe289c90e
Bruce Xu <ganyu.x@leantaas.com>
2020-04-22 23:22:45 -0700

    Created user registration form; Django tutorial part 6

--- anniversary_project/archive/models.py
+++ anniversary_project/archive/models.py
@@ -1,3 +1,17 @@
-from django.db import models
+import uuid
 
-# Create your models here.
+from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
+
+
+class Archive(models.Model):
+    archive_id = models.CharField(max_length=64, default=str(uuid.uuid4()), primary_key=True)
+    file_full_name = models.CharField(max_length=512, null=False)
+    archive_name = models.CharField(max_length=512, null=True)
+    owner_id = models.ForeignKey(to=User, on_delete=models.CASCADE)
+    date_created = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return self.archive_id
+
--- anniversary_project/users/views.py
+++ anniversary_project/users/views.py
@@ -1,8 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpRequest, HttpResponse
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
+
+from .forms import UserRegisterForm
 
 
 def register(request: HttpRequest) -> HttpResponse:
-    user_creation_form = UserCreationForm()
+    if request.method == 'POST':
+        user_creation_form = UserRegisterForm(request.POST)
+        if user_creation_form.is_valid():
+            #   Save the newly created user
+            user_creation_form.save()
+
+            #   Display a flash message for user creation
+            username = user_creation_form.cleaned_data.get('username')
+            messages.success(request, f"user {username} created")
+
+            #   After user creation, redirect to archive-home page
+            return redirect('archive-home')
+    else:
+        user_creation_form = UserRegisterForm()
+
     return render(request, 'users/register.html', {'user_creation_form': user_creation_form})
