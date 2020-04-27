b9e56e771f6d0f942247cb0b7008a0a53e6a5870
xuganyu96 <xuganyu@berkeley.edu>
2020-04-26 23:30:49 -0700

    Implemented archive and transferjob models

--- anniversary_project/archive/models.py
+++ anniversary_project/archive/models.py
@@ -9,9 +9,26 @@ class Archive(models.Model):
     archive_id = models.CharField(max_length=64, default=str(uuid.uuid4()), primary_key=True)
     file_full_name = models.CharField(max_length=512, null=False)
     archive_name = models.CharField(max_length=512, null=True)
-    owner = models.ForeignKey(to=User, on_delete=models.CASCADE)
+    owner: User = models.ForeignKey(to=User, on_delete=models.CASCADE)
     date_created = models.DateTimeField(default=timezone.now)
 
     def __str__(self):
-        return self.archive_id
+        return self.archive_id + 'owned by ' + self.owner.username
+
+
+class PersistentFileTransfer(models.Model):
+    batch_id = models.CharField(max_length=64, default=str(uuid.uuid4()), primary_key=True)
+    archive: Archive = models.ForeignKey(to=Archive, on_delete=models.DO_NOTHING)
+    file_part_index = models.IntegerField(null=False)
+    transfer_type = models.CharField(max_length=512, null=False)
+    status = models.CharField(max_length=512, null=False)
+    date_created = models.DateTimeField(default=timezone.now, null=False)
+    date_started = models.DateTimeField(null=True)
+    date_completed = models.DateTimeField(null=True)
+
+    class Meta:
+        unique_together = (('batch_id', 'archive', 'file_part_index'),)
+
+    def __str__(self):
+        return self.archive.archive_id + ':' + self.batch_id + ':' + str(self.file_part_index)
 
--- anniversary_project/archive/views.py
+++ anniversary_project/archive/views.py
@@ -2,12 +2,15 @@ from django.shortcuts import render
 from django.http import HttpResponse, HttpRequest
 from django.contrib.auth.decorators import login_required
 
+from .models import Archive
+
 
 #   TODO:
 #   Home should be made login only, and should only display the archives created by the user currently logged in
 @login_required
 def home(request: HttpRequest) -> HttpResponse:
     cur_user = request.user
+
     return render(request,
                   'archive/home.html',
                   context={'title': 'Home'})
