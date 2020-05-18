5f2c0f8951cdbe99dd8e39067cbce22030a18ff1
xuganyu96 <xuganyu@berkeley.edu>
2020-05-17 23:21:00 -0700

    Designed basic views with no inner workings

--- anniversary_project/admintools/views.py
+++ anniversary_project/admintools/views.py
@@ -3,3 +3,15 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'admintools/home.html')
+
+
+def detail(request, tool_id: str):
+    return render(request, 'admintools/admintool_detail.html')
+
+
+def develop(request):
+    return render(request, 'admintools/admintool_develop.html')
+
+
+def deploy(request):
+    return render(request, 'admintools/admintool_deploy.html')
